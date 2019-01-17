54b4e7079b4154bc6f57d62a43c72bccfbb109de
Ryan Martin <ryan.martin@axiell.com>
2019-01-17 23:05:17 +0000

    Write out to /dev/null

--- benchmark/src/main/scala/main/Main.scala
+++ benchmark/src/main/scala/main/Main.scala
@@ -1,7 +1,7 @@
 package com.axiell.benchmark
 
 import cats.effect._
-import cats.effect.concurrent.Deferred
+import cats.effect.concurrent._
 import cats.implicits._
 
 import com.sksamuel.elastic4s.http._
@@ -11,13 +11,17 @@ import com.sksamuel.elastic4s.searches.SearchRequest
 import com.sksamuel.elastic4s.searches.sort.FieldSort
 import com.sksamuel.elastic4s.searches.sort.SortOrder.Asc
 
+import java.nio.file.Paths
+
 import fs2._
+import fs2.text
+import fs2.io
 import fs2.concurrent.Queue
 
 import scala.language.higherKinds
 
 object Main extends IOApp {
-  class ElasticStream[T[_]]
+  case class ElasticStream[T[_]]
       (table: String, d: Deferred[T, Either[Throwable,Unit]]) {
     val client = ElasticClient(
       ElasticProperties("http://localhost:9200?cluster.name=elasticsearch")
@@ -73,20 +77,27 @@ object Main extends IOApp {
   }
 
   def prog[T[_]](table: String)
-      (implicit F: ConcurrentEffect[T]): T[ExitCode] = {
+      (implicit F: ConcurrentEffect[T], cs: ContextShift[T]): T[ExitCode] = {
+    val ec = scala.concurrent.ExecutionContext.global
+
     for {
          start <- F.delay(System.nanoTime)
       deferred <- Deferred[T, Either[Throwable,Unit]]
-        client =  new ElasticStream[T](table, deferred)
+        client =  ElasticStream[T](table, deferred)
         stream <- records[T](client)
                     .interruptWhen(deferred)
-                    .compile.drain.map { _ =>
-                      client.client.close()
-                      ExitCode.Success
-                    }
+                    .map(_.toString)
+                    .through(text.utf8Encode)
+                    .through(
+                      io.file.writeAll(Paths.get("/dev/null"), ec)
+                    )
+                    .compile
+                    .drain
+                    .map { _ => ExitCode.Success }
            end <- F.delay(System.nanoTime)
        elapsed =  (end - start).toDouble
              _ =  println(s"Took ${elapsed/1000000000.0}s")
+             _ =  client.client.close()
     } yield stream
   }
 
