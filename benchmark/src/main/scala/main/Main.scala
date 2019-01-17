8457d39e10ae1a766fba0a1c93d4566f13d5cf0f
Ryan Martin <ryan.martin@axiell.com>
2019-01-17 22:18:08 +0000

    GetAll

--- benchmark/src/main/scala/main/Main.scala
+++ benchmark/src/main/scala/main/Main.scala
@@ -1,27 +1,85 @@
 package com.axiell.benchmark
 
 import cats.effect._
+import cats.effect.concurrent.Deferred
+import cats.implicits._
 
-import com.sksamuel.elastic4s.http.{ElasticClient, ElasticProperties}
+import com.sksamuel.elastic4s.http._
 import com.sksamuel.elastic4s.http.ElasticDsl._
+import com.sksamuel.elastic4s.http.search.SearchResponse
+import com.sksamuel.elastic4s.searches.SearchRequest
+import com.sksamuel.elastic4s.searches.sort.FieldSort
+import com.sksamuel.elastic4s.searches.sort.SortOrder.Asc
+
+import fs2._
+import fs2.concurrent.Queue
+
+import scala.language.higherKinds
 
 object Main extends IOApp {
-	def run(args: List[String]): IO[ExitCode] = {
-		val client = ElasticClient(
+  class ElasticStream[T[_]](d: Deferred[T, Either[Throwable,Unit]]) {
+    val client = ElasticClient(
       ElasticProperties("http://localhost:9200?cluster.name=elasticsearch")
     )
 
-		IO.fromFuture {
-			IO {
-				client.execute {
-					searchWithType("nhm-eparties" -> "_doc")
-						.matchAllQuery
-						.size(10)
-				}
-			}
-		}.map { f =>
-			println(f)
-			ExitCode.Success
-		}
+    val baseQuery = searchWithType("nhm-eregistry" -> "_doc")
+                      .matchAllQuery
+                      .size(5000)
+                      .sortBy(
+                        FieldSort("id.keyword").order(Asc)
+                      )
+
+    def request(search: SearchRequest)(implicit F: Concurrent[T])
+        : T[Response[SearchResponse]] = {
+      IO.fromFuture(IO(client.execute(search))).to[T]
+    }
+
+    def nextQuery(sort: Option[Seq[AnyRef]]): SearchRequest = sort match {
+      case Some(s) => baseQuery.searchAfter(s)
+      case None => baseQuery
+    }
+
+    def data
+        (cb: Response[SearchResponse] => Unit, sort: Option[Seq[AnyRef]] = None)
+        (implicit F: Concurrent[T]): T[Unit] = {
+      request(nextQuery(sort))
+        .flatMap {
+          case f @ RequestFailure(_, _, _, _) =>
+            F.delay(cb(f))
+          case f @ RequestSuccess(_, _, _, result) =>
+            F.delay(cb(f))
+              .flatMap { _ =>
+                if (result.hits.hits.nonEmpty) {
+                  data(cb, result.hits.hits.last.sort)
+                } else {
+                  d.complete(Right(()))
+                }
+              }
+        }
+    }
+  }
+
+  def records[T[_]](es: ElasticStream[T])(implicit F: ConcurrentEffect[T])
+      : Stream[T, Response[SearchResponse]] = {
+    for {
+      q <- Stream.eval(Queue.unbounded[T, Response[SearchResponse]])
+      _ <- Stream.eval {
+        es.data(r => F.runAsync(q.enqueue1(r))(_ => IO.unit).unsafeRunSync)
+      }
+      row <- q.dequeue
+    } yield row
+  }
+
+	def run(args: List[String]): IO[ExitCode] = {
+    for {
+      deferred <- Deferred[IO, Either[Throwable,Unit]]
+        client =  new ElasticStream[IO](deferred)
+        stream <- records[IO](client)
+                    .interruptWhen(deferred)
+                    .compile.drain.map { _ =>
+                      client.client.close()
+                      ExitCode.Success
+                    }
+    } yield stream
 	}
 }
